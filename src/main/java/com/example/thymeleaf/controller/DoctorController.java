26a70c5995475978024bbe74c22bf0a0267bb002
duvaudierje <jerome.duvaudier@natixis.com>
2021-05-25 18:36:22 +0200

    Resolution quete

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,16 +1,23 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.thymeleaf.model.Doctor;
 
 @Controller
 public class DoctorController {
 
     @GetMapping("/doctor/")
     public String doctor(@RequestParam(required=false, defaultValue="0") int number,
-                         @RequestParam(required=false, defaultValue="John Smith") String name) {
+                         @RequestParam(required=false, defaultValue="John Smith") String name,
+                         Model model) {
+        
+        Doctor doc = new Doctor(number, name);
 
+        model.addAttribute("doctor", doc);
         return "doctor";
     }
 }
